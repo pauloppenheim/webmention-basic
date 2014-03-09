32d779128a822ebf4728771652c3aed31449e57c
Paul Oppenheim <paul@pauloppenheim.com>
2014-03-08 16:41:32 -0800

    initial commit

---
+++ README.rst
@@ -0,0 +1,35 @@
+
+webmention-basic
+================
+
+Trivial listener for `IndieWeb <http://indiewebcamp.com>`_ `webmentions <http://webmention.org>`_ in python. Simply receives and logs calls, does not implement verification, parsing, or anything else at all, really. If you want that, check out `Dainin <https://github.com/bear/dainin>`_ or `Ronkyuu <https://github.com/bear/ronkyuu>`_.
+
+
+
+webmention_basic.py
+===================
+
+A WSGI / CGI app that will listen for a POST. No examination of the path is done; assuming that is managed outside of the app.
+
+
+
+Roadmap
+=======
+
+* get working
+
+
+
+Contributors
+============
+
+* `Paul Oppenheim <http://pauloppenheim.com>`_
+
+
+
+Requires
+========
+Python v2.7 but see `requirements.txt`_ for a full list
+
+
+
---
+++ webmention_basic.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+:copyright: (c) 2014 by Paul Oppenheim
+:license: MIT, see LICENSE for more details.
+
+Trivial listener for
+`IndieWeb <http://indiewebcamp.com>`_ `webmentions <http://webmention.org>`_
+in python.
+
+  webmention
+"""
+
+import sys
+import cgi
+
+# must be called "application" to be run by mod_wsgi
+def application(environ, start_response):
+    #status = '200 OK'
+    #headers = [('Content-type', 'text/html; charset=UTF-8')]
+    status = '202 Accepted'
+    headers = []
+    body = ""
+    
+    form = cgi.FieldStorage(environ=environ, fp=environ["wsgi.input"])
+    
+    source=form.getfirst("source").strip()
+    target=form.getfirst("target").strip()
+    if source is None or target is None:
+        status = '400 Bad Request'
+        headers = [('Content-type', 'text/html; charset=UTF-8')]
+        body = "source or target form params missing\n"
+    print >> sys.stderr, "%s\t%s" % (source, target)
+    
+    start_response(status, headers)
+    return [body]
+
+# and CGI just runs as a shell executable
+if "__main__" == __name__:
+    from wsgiref.handlers import CGIHandler
+    CGIHandler().run(application)
+
+
